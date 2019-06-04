8215fd536d11fafaa2d2adb2a7225dfe3ac9d198
Jessie <Jessie.Bogart@yum.com>
2019-06-03 19:31:38 -0500

    Updates

---
+++ precache-manifest.5ba45553b548b038e35aeca8ee8c00c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6993cfb9dcc70be757d31d1c777dc043",
+    "url": "/MACG/index.html"
+  },
+  {
+    "revision": "eefcb3b64fc494ab8618",
+    "url": "/MACG/static/css/main.007e91c2.chunk.css"
+  },
+  {
+    "revision": "bdc426c3d81b3cc97d92",
+    "url": "/MACG/static/js/2.8fa3d35f.chunk.js"
+  },
+  {
+    "revision": "eefcb3b64fc494ab8618",
+    "url": "/MACG/static/js/main.5a709214.chunk.js"
+  },
+  {
+    "revision": "3fe0dcd74a19b4ce5ce1",
+    "url": "/MACG/static/js/runtime~main.15bd3290.js"
+  },
+  {
+    "revision": "254c47d5879554f3be10e3590628b400",
+    "url": "/MACG/static/media/vote.254c47d5.jpeg"
+  }
+]);
